ed94abd6b347964ebc8785b4da2b13e0fdc681ed
Sulav Poudel <87256163+SulavPoudel12@users.noreply.github.com>
2021-08-30 09:37:06 +0545

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,53 @@
+console.log ('hi')
+
+function navig() {
+    // nbbr = document.getElementsByClassName('ham')
+    nbbr = document.querySelector('ul')
+    console.log('no')
+    
+
+
+        nbbr.style.display= 'block'
+        hey = document.getElementById('ham1')
+        hey.style.display = 'none'
+        
+        hi = document.getElementById('cross')
+        hi.style.display = 'block'
+}
+
+function navig1(){
+
+    console.log('fire')
+
+
+    nbbr = document.querySelector('ul')
+    hey.style.display = 'block'
+    nbbr.style.display = "none" 
+
+    hey = document.getElementById('ham1')
+    
+    hi = document.getElementById('cross')
+    hi.style.display = 'none'
+
+}
+
+// setInterval(() => {
+     
+    
+//     if (x = window.matchMedia("(max-width: 730px)")){
+
+//         hey = document.getElementById('ham1')
+    
+//     hi = document.getElementById('cross')
+
+//     hi.style.display = 'none'
+//     hey.style.display = 'none'
+    
+//     ull = document.querySelector('ul')
+
+//     ull.style.display = 'block'
+
+//     }
+     
+// }, 1);
+// // navig()
